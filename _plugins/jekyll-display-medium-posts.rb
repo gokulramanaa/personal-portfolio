b361da4ac2820a3bd76a08ec9af9b5d95a7804d4
Gokulramanan Soundararajan <31602253+gokulramanaa@users.noreply.github.com>
2022-12-17 19:10:40 +0000

    update

--- _plugins/jekyll-display-medium-posts.rb
+++ _plugins/jekyll-display-medium-posts.rb
@@ -37,28 +37,3 @@ def generate(site)
     end
   end
 end
-
-# require 'feedjira'
-# module Jekyll
-#   class MediumPostDisplay < Generator
-#     safe true
-#     priority :high
-# def generate(site)
-#       jekyll_coll = Jekyll::Collection.new(site, 'medium_posts')
-#       site.collections['medium_posts'] = jekyll_coll
-# Feedjira::Feed.fetch_and_parse("https://medium.com/feed/@gokulramanaa").entries.each do |e|
-#         p "Title: #{e.title}, published on Medium #{e.url} #{e}"
-#         title = e[:title]
-#         content = e[:content]
-#         guid = e[:url]
-#         path = "./_medium_posts/" + title + ".md"
-#         path = site.in_source_dir(path)
-#         doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
-#         doc.data['title'] = title;
-#         doc.data['feed_content'] = content;
-#         jekyll_coll.docs << doc
-#         puts {{ doc || inspect }}
-#       end
-#     end
-#   end
-# end
